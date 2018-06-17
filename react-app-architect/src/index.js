c35f4627fbed14d858886cb2c22c00e3a86fd132
long.le <xuanlongvts@gmail.com>
2018-06-17 10:21:55 +0700

    fix index.js

--- react-app-architect/src/index.js
+++ react-app-architect/src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
-import logo from "./images/logo.svg";
+import logo from './images/logo.svg';
 import './styles/index.css';
 
-import { Provider } from 'react-redux';
 import store from './app/stores';
 import Routes from './app/routers';
 
 ReactDOM.render(
-    <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+    <div className='App'>
+        <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <h1 className='App-title'>Welcome to React</h1>
         </header>
         <Provider store={store()}>
             <Routes />
