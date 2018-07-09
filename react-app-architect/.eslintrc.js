10f982bd5982656fa4436b5275f19c5965a87544
long.le <xuanlongvts@gmail.com>
2018-07-09 23:24:23 +0700

    eslint config

--- react-app-architect/.eslintrc.js
+++ react-app-architect/.eslintrc.js
@@ -35,6 +35,8 @@ module.exports = {
         'react/no-array-index-key': 0,
         'react/prefer-stateless-function': 0,
         'react/forbid-prop-types': 0,
+        'react/jsx-one-expression-per-line': 0,
+        'react/button-has-type': 0,
 
         'jsx-a11y/click-events-have-key-events': 0,
         'jsx-a11y/no-static-element-interactions': 0,
