f62107f948364e88eab7a32315fe0d4f9abe4d27
long.le <xuanlongvts@gmail.com>
2018-07-27 23:52:33 +0700

    infor cus

--- leflair/src/app/components/checkout/form.js
+++ leflair/src/app/components/checkout/form.js
@@ -1,59 +1,146 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Field, reduxForm } from 'redux-form';
-import validate from './validate';
 
-const renderField = ({ input, id, label, type, meta: { touched, error, warning } }) => (
-    <div className="eachRow">
-        <label htmlFor={id} className="lbl">
-            {label} :
-        </label>
-        <div className="boxVal">
-            <input {...input} placeholder={label} type={type} id={id} />
-            {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
-        </div>
-    </div>
-);
+import validate, { customerInfo } from './validate';
 
-const CheckoutForm = ({ pristine, reset, submitting, handleSubmit }) => {
+const renderField = ({ input, id, label, type, valueDefault, isFill, meta: { touched, error, warning } }) => {
+    isFill && (input.value = valueDefault);
     return (
-        <div className="container">
-            <h2 className="titlePage">Customer's information</h2>
-            <form onSubmit={handleSubmit} className="frmCheckout">
-                <Field name="fullname" type="text" id="fullname" component={renderField} label="Full name" />
-                <Field name="email" type="email" id="email" component={renderField} label="Email" />
-                <Field name="phone" type="number" id="phone" component={renderField} label="Phone number" />
-                <Field name="address" type="text" id="address" component={renderField} label="Address" />
-                <div className="actForm">
-                    <button type="submit" disabled={submitting}>
-                        Continue
-                    </button>
-                    <button type="button" disabled={pristine || submitting} onClick={reset}>
-                        Clear Values
-                    </button>
-                </div>
-            </form>
+        <div className="eachRow">
+            <label htmlFor={id} className="lbl">
+                {label} :
+            </label>
+            <div className="boxVal">
+                <input {...input} placeholder={label} type={type} id={id} disabled={isFill ? true : false} />
+                {!isFill && touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+            </div>
         </div>
     );
 };
 
+class CheckoutForm extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isFill: false
+        };
+    }
+
+    handleFillInfor(e) {
+        this.setState({
+            isFill: e.target.checked
+        });
+    }
+
+    handleSubmitFrm(e, isFill) {
+        e.preventDefault();
+
+        const { onSubmit, handleSubmit } = this.props;
+
+        if (isFill) {
+            onSubmit(customerInfo);
+        } else {
+            handleSubmit(null);
+        }
+    }
+
+    render() {
+        const { isFill } = this.state;
+        const { pristine, reset, submitting } = this.props;
+
+        let isDataFull = 0;
+        for (let key in customerInfo) {
+            if (customerInfo[key]) {
+                ++isDataFull;
+            }
+        }
+
+        return (
+            <div className="container">
+                <h2 className="titlePage">Customer's information</h2>
+                {isDataFull === 4 ? (
+                    <div className="fillInfo">
+                        <label>
+                            Fill old information: <input type="checkbox" onClick={e => this.handleFillInfor(e)} />
+                        </label>
+                    </div>
+                ) : null}
+
+                <form onSubmit={e => this.handleSubmitFrm(e, isFill)} className="frmCheckout">
+                    <Field
+                        name="fullname"
+                        type="text"
+                        id="fullname"
+                        component={renderField}
+                        label="Full name"
+                        valueDefault={customerInfo.fullname}
+                        isFill={isFill}
+                    />
+                    <Field
+                        name="email"
+                        type="email"
+                        id="email"
+                        component={renderField}
+                        label="Email"
+                        valueDefault={customerInfo.email}
+                        isFill={isFill}
+                    />
+                    <Field
+                        name="phone"
+                        type="number"
+                        id="phone"
+                        component={renderField}
+                        label="Phone number"
+                        valueDefault={customerInfo.phone}
+                        isFill={isFill}
+                    />
+                    <Field
+                        name="address"
+                        type="text"
+                        id="address"
+                        component={renderField}
+                        label="Address"
+                        valueDefault={customerInfo.address}
+                        isFill={isFill}
+                    />
+                    <div className="actForm">
+                        <button type="submit" disabled={submitting}>
+                            Continue
+                        </button>
+
+                        <button type="button" disabled={pristine || submitting || isFill} onClick={reset}>
+                            Clear Values
+                        </button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
 CheckoutForm.propTypes = {
     handleSubmit: PropTypes.func,
+    onSubmit: PropTypes.func,
     pristine: PropTypes.bool,
     submitting: PropTypes.bool,
     reset: PropTypes.func
 };
 
 renderField.propTypes = {
+    valueDefault: PropTypes.string,
     value: PropTypes.string,
     label: PropTypes.string,
     id: PropTypes.string,
     type: PropTypes.string,
     meta: PropTypes.object,
-    input: PropTypes.object
+    input: PropTypes.object,
+    isFill: PropTypes.bool.isRequired
 };
 
 export default reduxForm({
     form: 'syncValidation',
-    validate
+    validate,
+    enableReinitialize: true
 })(CheckoutForm);
--- leflair/src/app/components/checkout/validate.js
+++ leflair/src/app/components/checkout/validate.js
@@ -1,27 +1,50 @@
+import localStogeAdapter from '../../_utils/localStorage';
+
+export const inforCus = 'inforCus';
+const getInforCus = localStogeAdapter.getItemJson(inforCus);
+export const customerInfo = {
+    fullname: getInforCus ? getInforCus.fullname : '',
+    email: getInforCus ? getInforCus.email : '',
+    address: getInforCus ? getInforCus.address : '',
+    phone: getInforCus ? getInforCus.phone : ''
+};
+
 const validate = values => {
     const errors = {};
     if (!values.fullname) {
         errors.fullname = 'Required';
     } else if (values.fullname.length < 3) {
         errors.fullname = 'Must be 3 characters or more';
+    } else {
+        customerInfo.fullname = values.fullname;
+        localStogeAdapter.setItemJson(inforCus, customerInfo);
     }
 
     if (!values.email) {
         errors.email = 'Required';
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
         errors.email = 'Invalid email address';
+    } else {
+        customerInfo.email = values.email;
+        localStogeAdapter.setItemJson(inforCus, customerInfo);
     }
 
     if (!values.address) {
         errors.address = 'Required';
     } else if (values.address.length < 10) {
         errors.address = 'Must be 10 characters or more';
+    } else {
+        customerInfo.address = values.address;
+        localStogeAdapter.setItemJson(inforCus, customerInfo);
     }
 
     if (!values.phone) {
         errors.phone = 'Required';
     } else if (values.phone && values.phone.length < 10) {
         errors.phone = 'Invalid phone number, must be 10 digits';
+    } else {
+        customerInfo.phone = values.phone;
+        localStogeAdapter.setItemJson(inforCus, customerInfo);
     }
 
     return errors;
